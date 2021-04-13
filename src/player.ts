61c24725a68d555119c19b893458dfcfca49b3e7
Matioma <Artiom.Matievschi@gmail.com>
2021-04-13 14:36:57 +0200

    Create camera

--- src/player.ts
+++ src/player.ts
@@ -4,15 +4,27 @@ import {
   Scene,
   ShadowGenerator,
   TransformNode,
+  UniversalCamera,
   Vector3,
 } from "@babylonjs/core";
 
 export class Player extends TransformNode {
-  public camera;
+  private static readonly ORIGINAL_TILT: Vector3 = new Vector3(
+    0.5934119456780721,
+    0,
+    0
+  );
+
+  private _yTilt;
+
+  public camera: UniversalCamera;
+
   public scene: Scene;
 
   private _input;
   public mesh: Mesh;
+
+  private _camRoot;
 
   constructor(assets, scene: Scene, shadowGenerator: ShadowGenerator, input?) {
     super("player", scene);
@@ -26,14 +38,36 @@ export class Player extends TransformNode {
     this._input = input;
   }
 
-  _setupPlayerCamera() {
-    var camera4 = new ArcRotateCamera(
-      "arc",
-      -Math.PI / 2,
-      Math.PI / 2,
-      40,
-      new Vector3(0, 3, 0),
+  _setupPlayerCamera(): UniversalCamera {
+    this._camRoot = new TransformNode("root");
+    this._camRoot.position = new Vector3(0, 0, 0);
+    this._camRoot.rotation = new Vector3(0, Math.PI, 0);
+
+    let yTilt = new TransformNode("ytilt");
+    yTilt.rotation = Player.ORIGINAL_TILT;
+
+    this._yTilt = yTilt;
+    yTilt.parent = this._camRoot;
+
+    this.camera = new UniversalCamera(
+      "cam",
+      new Vector3(0, 0, -30),
       this.scene
+    );
+    this.camera.lockedTarget = this._camRoot.position;
+    this.camera.fov = 0.47350045992678597;
+    this.camera.parent = yTilt;
+
+    this.scene.activeCamera = this.camera;
+    return this.camera;
+  }
+
+  private _updateCamera(): void {
+    let cameraPlayer: number = this.mesh.position.y + 2;
+    this._camRoot.position = Vector3.Lerp(
+      this._camRoot.position,
+      new Vector3(this.mesh.position.x, cameraPlayer, this.mesh.position.z),
+      0.4
     );
   }
 }
